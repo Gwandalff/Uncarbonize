afdde9382bc620802ccd25ae16fa8d4aab066b97
Gwendal Jouneaux <gwendal.jouneaux@.irisa.fr>
2020-07-06 15:57:47 +0200

    fix bugs

--- Uncarbonize_Proxy/uncarbonize.js
+++ Uncarbonize_Proxy/uncarbonize.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 const request = require('request');
 const express = require('express');
+const { JSDOM } = require("jsdom");
 const zlib = require('zlib');
 const _url = require('url');
 const fs = require('fs');
@@ -8,13 +9,28 @@ const app = express();
 const port = 8080;
 
 
-const sendBody = (res) => (page) => {
+const sendBody = (res) => (page, zipped) => {
     
     console.log("proxy: HTTP intercept response: MODIFIED RESPONSE BODY");
-    res.send(page);
+    if(zipped){
+	console.log("Send Zipped");
+	zlib.gzip(page, function (err, archive) {
+		if (err){return res.send(page);}
+		res.header('Content-Encoding', 'gzip');
+		res.header('vary', 'Accept-Encoding');
+		res.header('access-control-allow-origin', '*');
+		res.header('content-type', 'text/html; charset=utf-8');
+		res.send(archive);
+		return;
+	});
+    } else {
+	res.send(page);
+    }
 }
 
-const process = (req, origin, send) => (body) => {
+const process = (req, origin, send) => (body, zipped) => {
+	const dom = new JSDOM(body);
+	body = dom.window.document.documentElement.outerHTML
 	const page = body.replace(/<head>/g, '<head><base href="' + origin + '">');
 
         fs.writeFileSync('./input.html', page, 'utf-8')
@@ -22,6 +38,8 @@ const process = (req, origin, send) => (body) => {
 	command = command + "; export ADAPTABLE_HTML_ACCURACY="+req.params.accuracy;
 	command = command + "; html ./input.html --conditional-loading --loop-perforation";
 
+	console.log("VM start");
+	//console.log(page);
         exec(command, (error, stdout, stderr) => { // --conditional-loading --loop-perforation --degrade-image --degrade-image.folder=\"/home/ImageDegradation/\"
         if (error) {
             console.log(`error: ${error.message}`);
@@ -33,7 +51,7 @@ const process = (req, origin, send) => (body) => {
             //send(page);
             //return
         }
-        send(stdout);
+        send(stdout, zipped);
         });
 }
 
@@ -46,19 +64,24 @@ app.get('/uncarbonize/:energy/:accuracy/:url', (req, res) => {
     const send = sendBody(res);
     const callback = process(req, origin, send);
 
-    request({url:uri, encoding: null}, (err, response, body) => {
+    request({url:uri, headers:{"Accept-Encoding":"gzip"}, encoding: null}, (err, response, body) => {
         if (err) { return console.log(err); }
 
         console.log("PROXY : " + uri);
 
-	if(body.indexOf("<html") > -1){
-		callback(body.toString());
+	const zipped = response.headers["content-encoding"] && response.headers["content-encoding"].includes("gzip");
+
+	console.log("Zipped : " + zipped);
+
+	if(!zipped){
+		callback(body.toString(), zipped);
 	} else {
 		zlib.gunzip(body, function(err, dezipped) {
 			if(err){
-				callback(body.toString());
+				console.log("Unzip Errpr");
+				callback(body.toString(), zipped);
 			} else {
-				callback(dezipped.toString());
+				callback(dezipped.toString(), zipped);
 			}
 		});
 	}
