799d9ac89f914eff6616c5a0cfb9b8db1ce703c5
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2020-07-17 08:48:19 +0200

    optimize proxy

--- Uncarbonize_Proxy/uncarbonize.js
+++ Uncarbonize_Proxy/uncarbonize.js
@@ -8,6 +8,8 @@ const fs = require('fs');
 const app = express();
 const port = 8080;
 
+var last_request = "";
+var last_request_zipped = false;
 
 const sendBody = (res) => (page, zipped) => {
     
@@ -28,19 +30,14 @@ const sendBody = (res) => (page, zipped) => {
     }
 }
 
-const process = (req, origin, send) => (body, zipped) => {
-	const dom = new JSDOM(body);
-	body = dom.window.document.documentElement.outerHTML
-	const page = body.replace(/<head>/g, '<head><base href="' + origin + '">');
-
-        fs.writeFileSync('./input.html', page, 'utf-8')
+const exec_command = (req, origin, send) => (zipped) => {
 	let command =         "export ADAPTABLE_HTML_ENERGY="+req.params.energy;
 	command = command + "; export ADAPTABLE_HTML_ACCURACY="+req.params.accuracy;
-	command = command + "; html ./input.html --conditional-loading --loop-perforation";
+	command = command + "; html ./input.html --conditional-loading --loop-perforation"// --degrade-image --degrade-image.origin=\""+origin+"\" --degrade-image.folder=\"/home/gwandalf/ImageDegradation/\"";
 
 	console.log("VM start");
 	//console.log(page);
-        exec(command, (error, stdout, stderr) => { // --conditional-loading --loop-perforation --degrade-image --degrade-image.folder=\"/home/ImageDegradation/\"
+        exec(command, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             //send(page);
@@ -55,6 +52,18 @@ const process = (req, origin, send) => (body, zipped) => {
         });
 }
 
+const process = (req, origin, send) => (body, zipped) => {
+	const launch = exec_command(req, origin, send);
+	const dom = new JSDOM(body);
+	const base = dom.window.document.createElement("BASE");
+	base.setAttribute("href",origin)
+	dom.window.document.documentElement.children[0].prepend(base)
+	body = dom.window.document.documentElement.outerHTML
+
+        fs.writeFileSync('./input.html', body, 'utf-8')
+	launch(zipped);
+}
+
 app.get('/uncarbonize/:energy/:accuracy/:url', (req, res) => {
 
     const uri = decodeURIComponent(req.params.url);
@@ -64,12 +73,21 @@ app.get('/uncarbonize/:energy/:accuracy/:url', (req, res) => {
     const send = sendBody(res);
     const callback = process(req, origin, send);
 
+    if(uri === last_request){
+	const launch = exec_command(req, origin, send);
+	launch(last_request_zipped);
+	return;
+    }
+
     request({url:uri, headers:{"Accept-Encoding":"gzip"}, encoding: null}, (err, response, body) => {
         if (err) { return console.log(err); }
 
         console.log("PROXY : " + uri);
 
 	const zipped = response.headers["content-encoding"] && response.headers["content-encoding"].includes("gzip");
+
+	last_request = uri;
+	last_request_zipped = zipped;
 
 	console.log("Zipped : " + zipped);
 
